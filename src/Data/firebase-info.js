ee83d12f505bf245462dc7d1a66cefc3cfe98473
amanfoundongithub <99312253+amanfoundongithub@users.noreply.github.com>
2022-12-10 10:56:23 +0530

    No changes

--- src/Data/firebase-info.js
+++ src/Data/firebase-info.js
@@ -8,15 +8,17 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// Fill these up yourself 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_7w2txuE9Yjs-Z3MfgNXkcQtHNXCm8vM",
-  authDomain: "todolist-1b3df.firebaseapp.com",
-  projectId: "todolist-1b3df",
-  storageBucket: "todolist-1b3df.appspot.com",
-  messagingSenderId: "217872194189",
-  appId: "1:217872194189:web:e3304b377a38b34c2360c4",
-  measurementId: "G-QDQNSMXQPS",
-  databaseURL: "https://todolist-1b3df-default-rtdb.asia-southeast1.firebasedatabase.app/"
+  apiKey: ,
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: 
+  measurementId: ,
+  databaseURL: 
 };
 
 // Initialize Firebase
